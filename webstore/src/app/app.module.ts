524609921fc89b419fcd7d1d610db11890f08483
Tylor Krafjack <takrafjack3487@eagle.fgcu.edu>
2023-04-10 23:04:29 -0400

    fixed app.module.ts error

--- webstore/src/app/app.module.ts
+++ webstore/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './login/login/login.component';
 import { SignupComponent } from './signup/signup/signup.component';
 import { HomeComponent } from './home/home/home.component';
 
